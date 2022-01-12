504e5fd7b94af86abc3db19b228e0e5ded658dcd
kkrizek06 <96017493+kkrizek06@users.noreply.github.com>
2022-01-11 19:31:30 -0500

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -118,7 +118,7 @@ with open(file_to_save, "w") as txt_file:
     # 7: Print the county with the largest turnout to the terminal.
     winning_county_summary = (
         f"--------------------------------\n"
-        f"winning county: {winning_county}\n"
+        f"Largest County Turnout: {winning_county}\n"
         f"--------------------------------\n")
     
     print(winning_county_summary)
